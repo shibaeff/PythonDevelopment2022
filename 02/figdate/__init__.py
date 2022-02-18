20c3ec042af6de8f777d2817a76fd783bd32ab68
paavo <shibaev.pp@phystech.edu>
2022-02-19 00:44:52 +0300

    implement init

--- 02/figdate/__init__.py
+++ 02/figdate/__init__.py
@@ -2,5 +2,4 @@ import time
 import pyfiglet 
 
 def figdate(format='%Y %d %b, %A', font='graceful'):
-    time_str = time.strftime(format)
-    return pyfiglet.Figlet(font).renderText(time_str)
+    return pyfiglet.Figlet(font).renderText(time.strftime(format))
