c4e75c0a2a27ab6f03be5e87a8baef47d78f5ac6
paavo <shibaev.pp@phystech.edu>
2022-03-02 21:42:41 +0300

    implement gameplay

--- 03_MergeRequirements/bullscows/__init__.py
+++ 03_MergeRequirements/bullscows/__init__.py
@@ -6,3 +6,15 @@ def bullscows(guess: str, secret: str) -> (int, int):
     bulls = td.hamming.similarity(guess, secret)
     cows = td.bag.similarity(guess, secret)
     return (bulls, cows)
+
+def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
+    secret = random.choice(words)
+    guess = ''
+    b = 1
+    cntr = 0
+    while b != len(secret) or b != len(guess):
+        guess = ask("Введите слово: ", words)
+        cntr += 1
+        b, c = bullscows(guess, secret)
+        inform("Быки: {}, Коровы: {}", b, c)
+    return cntr
