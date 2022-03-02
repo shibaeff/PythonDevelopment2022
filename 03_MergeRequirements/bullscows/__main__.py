cea96047d05837bec70b60408bb46fe500297c7a
paavo <shibaev.pp@phystech.edu>
2022-03-02 21:57:01 +0300

    implement main

--- 03_MergeRequirements/bullscows/__main__.py
+++ 03_MergeRequirements/bullscows/__main__.py
@@ -12,3 +12,25 @@ def ask(prompt: str, valid: list[str] = None) -> str:
 
 def inform(format_string: str, bulls: int, cows: int) -> None:
     print(format_string.format(bulls, cows))
+
+if len(sys.argv) < 2:
+    fatal()
+
+dictionary_link = sys.argv[1]
+length = sys.argv[2] if len(sys.argv) > 2 else 5
+
+try_url = False
+try:
+    with open(dictionary_link) as f:
+        dictionary = f.read()
+except FileNotFoundError:
+    try_url = True
+
+if try_url:
+    try:
+        resp = urllib.request.urlopen(dictionary_link)
+        dictionary = resp.read().decode()
+    except Exception:
+        fatal()
+
+words = dictionary.split()
