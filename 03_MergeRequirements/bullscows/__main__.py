0246d5a564a6cd3dd014d5598003da1521c23472
paavo <shibaev.pp@phystech.edu>
2022-03-02 21:55:35 +0300

    implement ask

--- 03_MergeRequirements/bullscows/__main__.py
+++ 03_MergeRequirements/bullscows/__main__.py
@@ -1,3 +1,12 @@
 from . import gameplay
 import sys
 import urllib.request
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    if valid is not None:
+        guess = input(prompt)
+    else:
+        while (guess := input(prompt)) not in valid:
+            continue
+    return guess
+
