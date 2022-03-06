a063c9f63222a85dbc649b16388af3b0b2b4869d
paavo <shibaev.pp@phystech.edu>
2022-03-06 21:17:21 +0300

    implement language

--- 04_MergetoolCommandline/main.py
+++ 04_MergetoolCommandline/main.py
@@ -3,3 +3,17 @@ import shlex
 import cmd
 import importlib
 from pynames import GENDER, LANGUAGE
+
+
+class Generator(cmd.Cmd):
+    lang = LANGUAGE.EN
+
+    def do_language(self, args):
+        if args == 'RU':
+            self.lang = LANGUAGE.RU
+        elif args == 'EN':
+            self.lang = LANGUAGE.EN
+        elif args == 'NATIVE':
+            self.lang = LANGUAGE.NATIVE
+
+    def do_generate(selfself, args):
