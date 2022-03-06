e85a4f48220efc65f543f8501324f93478af7daf
paavo <shibaev.pp@phystech.edu>
2022-03-06 22:16:38 +0300

    implement do_info

--- 04_MergetoolCommandline/main.py
+++ 04_MergetoolCommandline/main.py
@@ -16,4 +16,24 @@ class Generator(cmd.Cmd):
         elif args == 'NATIVE':
             self.lang = LANGUAGE.NATIVE
 
-    def do_generate(selfself, args):
+    def do_info(self, args):
+        arguments = shlex.split(args)
+        i = importlib.import_module('pynames.generators.' + arguments[0])
+        generator_objects = [s for s in dir(i) if s.endswith('NamesGenerator')]
+        match arguments:
+            case [_, 'language']:
+                for object_name in generator_objects:
+                    cls = eval(f'i.{object_name}')()
+                    print(f'class {object_name} has {cls.languages} languages')
+            case [_, _]:
+                for object_name in generator_objects:
+                    cls = eval(f'i.{object_name}')()
+                    match arguments[-1]:
+                        case 'male':
+                            print(f'class {object_name} has {cls.get_names_number(GENDER.MALE)} male names')
+                        case 'female':
+                            print(f'class {object_name} has {cls.get_names_number(GENDER.FEMALE)} female names')
+            case [_]:
+                for object_name in generator_objects:
+                    cls = eval(f'i.{object_name}')()
+                    print(f'class {object_name} has {cls.get_names_number()} names')
