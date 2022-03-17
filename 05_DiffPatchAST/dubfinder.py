fcc91d9e658cbd7a3546e427276efc1d29112bd2
paavo <shibaev.pp@phystech.edu>
2022-03-17 20:59:54 +0300

    add ast parsing

--- 05_DiffPatchAST/dubfinder.py
+++ 05_DiffPatchAST/dubfinder.py
@@ -2,25 +2,44 @@
 import inspect
 import sys
 import importlib
+import textwrap
 import ast
 
 def get_funcs(entity):
+    print(entity)
     subentities = inspect.getmembers(entity, inspect.ismodule)
-    subentities.extend(inspect.getmembers, inspect.ismodule)
+    subentities.extend(inspect.getmembers(entity, inspect.isclass))
     funcs = []
     for sub in subentities:
         funcs.extend(get_funcs(sub))
     return funcs
 
-def get_functions(module):
-    # print(f"importing module {module}")
-    mod = importlib.import_module(module)
-    members = inspect.getmembers(mod)
-    # print(f"got members: {members}")
-    funcs = inspect.getmembers(mod, inspect.isfunction)
-    inner_mods = inspect.getmembers(mod, inspect.ismodule)
-    print(funcs)
-    return funcs
+def get_functions(module, name):
+    members = inspect.getmembers(module)
+    functions = []
+    for elem in members:
+        if inspect.isfunction(elem[1]):
+            functions += [(f"{name}.{elem[0]}", elem[1]), ]  # такая схема - чтобы сохранить путь к функции
+        elif inspect.isclass(elem[1]):
+            if not elem[0].startswith('__'):
+                functions += get_functions(elem[1], f"{name}.{elem[0]}")
+    return functions
+
+def parse(fun):
+    code = textwrap.dedent(inspect.getsource(fun[1]))
+    tree = ast.parse(code)
+    nodes = ast.walk(tree)
+    for node in nodes:
+        if hasattr(node, "id"):
+            node.id = "_"
+        elif hasattr(node, "name"):
+            node.name = "_"
+        elif hasattr(node, "arg"):
+            node.arg = "_"
+        elif hasattr(node, "attr"):
+            node.attr = "_"
+    return fun[0], ast.unparse(tree)
+
 
 def astformat(node):
     if isinstance(node, ast.AST):
@@ -32,6 +51,7 @@ def astformat(node):
     elif isinstance(node, list):
         return "".join(astformat(x) for x in node)
     return str(node)
+
 
 def parse_function(func):
     source = inspect.getsource(func)
@@ -50,5 +70,6 @@ print(f"Got modules: {modules}")
 funcs = []
 for mod in modules:
     funcs.extend(get_functions(mod))
-for func in funcs:
+print(funcs)
+
     
